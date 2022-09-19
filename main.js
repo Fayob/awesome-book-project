76c8c233be553144aeb5cad70604f4e3c93344ec
= <adedejifavour6@gmail.com>
2022-09-19 18:50:05 +0100

    add awesome book functionality

---
+++ main.js
@@ -0,0 +1,9 @@
+const viewSection = document.querySelector("#view_section")
+const title = document.querySelector(".title")
+const author = document.querySelector(".author")
+
+const bookLists = []
+
+function addBook(id, title, author) {
+    
+}
