e027f6e2bc72fab8fdc8756b1eae61aba12a05b7
= <adedejifavour6@gmail.com>
2022-09-20 18:50:06 +0100

    Add render and localStorage functionality

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-const viewSection = document.getElementById('view_section');
-const addButton = document.querySelector('.button');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const bookForm = document.querySelector('.button');
+const displayBook = document.querySelector('#view_section');
 
-const bookLists = [];
-
-function addBook(title, author) {
-  bookLists.push({ title, author });
+let bookData;
+let bookStore = JSON.parse(localStorage.getItem('localBook')) || [];
+function addBook() {
+  bookData = {
+    title: title.value,
+    author: author.value,
+    id: Math.floor(Math.random() * 1000000),
+  };
+  bookStore.push(bookData);
+  localStorage.setItem('localBook', JSON.stringify(bookStore));
 }
 
-addButton.addEventListener('click', () => {
-  let titleValue = title.value;
-  let authorValue = author.value;
+function removeBook(id) {
+  bookStore = bookStore.filter((books) => books.id !== id);
+  localStorage.setItem('localBook', JSON.stringify(bookStore));
+}
 
-  if (titleValue.trim() === '') {
-    title.setCustomValidity('Please supply a title');
-    title.reportValidity();
-    return;
+function renderBook(bookData) {
+  const div = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const removeBtn = document.createElement('button');
+  const hr = document.createElement('hr');
+  bookTitle.innerText = bookData.title;
+  bookAuthor.innerText = bookData.author;
+  removeBtn.innerText = 'Remove';
+
+  div.append(bookTitle, bookAuthor, removeBtn, hr);
+  displayBook.append(div);
+  removeBtn.addEventListener('click', () => {
+    removeBtn.parentElement.remove();
+    removeBook(bookData.id);
+  });
+}
+
+bookStore.forEach(renderBook);
+
+bookForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (title.value !== '' && author.value !== '') {
+    addBook();
+    renderBook(bookData);
+    title.value = '';
+    author.value = '';
+  } else {
+    alert('Please fill in the fields');
   }
-  if (authorValue.trim() === '') {
-    author.setCustomValidity('Please supply an author');
-    author.reportValidity();
-    return;
-  }
-
-  addBook(titleValue, authorValue);
-
-  const newBook = document.createElement('article');
-  newBook.className = 'new_book';
-  newBook.innerHTML = `
-      <h2>${titleValue}</h2>
-      <h5>${authorValue}</h5>
-      <button class='remove_button'>Remove</button>
-      <hr>
-      `;
-  viewSection.append(newBook);
-  titleValue = '';
-  authorValue = '';
-  localStorage.setItem('userBook', JSON.stringify(bookLists);
 });
